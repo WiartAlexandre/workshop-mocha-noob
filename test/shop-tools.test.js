7c09df5b0cd4dc72cabb587ea0e45961338f6082
WiartAlexandre <alexandre.wiart@neo-se.fr>
2017-11-21 15:33:46 +0100

    exo1

---
+++ test/shop-tools.test.js
@@ -0,0 +1,20 @@
+
+    var expect = require('chai').expect;
+
+    var getPrices = require("../src/shop-tools").getPrices;
+
+    describe("getPrices", function() {
+        var products = getPrices();
+
+        it('should return an object which describes the prices of your products', function() {
+            expect(products).to.eql({
+                banana: 1,
+                potato: 2,
+                tomato: 3,
+                cucumber: 4,
+                salad: 5,
+                apple: 6
+            });
+        })
+
+    });
